779973e592d0985eef2c17e3f359c8bf7b67b49b
rathana-voy <rathanavoy@hotmail.com>
2016-12-06 20:52:50 +0700

    add new project navDrawer test

---
+++ app/src/main/java/com/api/rathana/navdrawer/MainActivity.java
@@ -0,0 +1,29 @@
+package com.api.rathana.navdrawer;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private NavigationView mNavView;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle actionToogle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
+
+
+
+    }
+}
